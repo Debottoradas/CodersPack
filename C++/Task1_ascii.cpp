97d6ef46568e57fd04918e89a604870f4181837a
Debottoradas <52678186+Debottoradas@users.noreply.github.com>
2019-07-19 13:31:37 +0530

    uploaded 1 fille

---
+++ C++/Task1_ascii.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	char ch;
+	cout << "enter any character: ";
+	cin >> ch;
+	i = ch;
+	cout << "the ascii value is: " << i;
+	return 0;
+}
